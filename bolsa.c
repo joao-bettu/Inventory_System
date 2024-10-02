35db08ea81fa185afb98f0cdcabbff722ba9a89c
Joao Bettu <joao.bettu@ixcsoft.com.br>
2024-10-02 08:00:49 -0300

    Cadastro de produtos

--- bolsa.c
+++ bolsa.c
@@ -2,21 +2,32 @@
 #include <stdlib.h>
 #include "itens.h"
 
-Produto cad_item(int id_count);
-Inventory add_item(Produto item, Sentinela sentry);
+Produto add_item(int id_count);
+Inventory add_to_list(Produto item, Sentinela sentry);
 void null_sentry(Sentinela sentry);
 int main(){
     
 
     return 0;
 }
-Produto cad_item(int id_count){
-    
+Produto add_item(int id_count){
+    Produto novo;
+
+    novo.id = id_count;
+    printf("Nome o produto: ");
+    scanf("%s", novo.nome);
+    printf("Descrição do produto: ");
+    scanf("%s", novo.descricao);
+    printf("Peso do produto: ");
+    scanf("%f", &novo.peso);
 }
-Inventory add_item(Produto item, Sentinela sentry){
+Inventory add_to_list(Produto item, Sentinela sentry){
     Inventory *atual, *aux;
 
     atual = (Inventory *)malloc(sizeof(Inventory));
+    for(aux=sentry.head;aux!=NULL;aux=aux->next){
+        
+    }
 }
 void null_sentry(Sentinela sentry){
 
